af8f383be6cb430cf49a0d003da768fdd7702d52
Marc Anthony Reyes <marc.reyes@mynt.xyz>
2021-01-31 21:16:40 +0800

    Make site based in eleventify

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addLayoutAlias('default', 'base.njk');
+    eleventyConfig.addPassthroughCopy("site/css");
+    eleventyConfig.addPassthroughCopy("site/images");
+
+    return {
+        dir: {
+            input: "site",
+            layouts: "_layouts",
+            output: "dist"
+        }
+    };
+};
